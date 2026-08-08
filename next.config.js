0ffd7a51cbc8a4777a33f568e6d0b5098634d906
iitssjstn <companydesignsfx@gmail.com>
2026-08-08 17:58:54 +0200

    Configure remote image patterns in next.config.js
    Add remote image patterns for external sources.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "pixabay.com" },
+      { protocol: "https", hostname: "cdn.pixabay.com" },
+    ],
+  },
 };
 
 module.exports = nextConfig;
