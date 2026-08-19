34bb5c09d00fefe201e8585be20e40748118e5e1
iitssjstn <companydesignsfx@gmail.com>
2026-08-19 17:45:12 +0200

    Add ezoicEnabled prop to ConsentGatedScripts

--- app/components/ConsentGatedScripts.js
+++ app/components/ConsentGatedScripts.js
@@ -3,7 +3,7 @@
 import Script from "next/script";
 import { useHasAdConsent } from "./useHasAdConsent";
 
-export default function ConsentGatedScripts({ adsenseClientId, socialBarUrl }) {
+export default function ConsentGatedScripts({ adsenseClientId, socialBarUrl, ezoicEnabled }) {
   const hasConsent = useHasAdConsent();
   if (!hasConsent) return null;
 
@@ -19,6 +19,14 @@ export default function ConsentGatedScripts({ adsenseClientId, socialBarUrl }) {
       )}
       {socialBarUrl && (
         <Script src={socialBarUrl} strategy="afterInteractive" />
+      )}
+      {ezoicEnabled && (
+        <>
+          <Script async src="https://www.ezojs.com/ezoic/sa.min.js" strategy="afterInteractive" />
+          <Script id="ezoic-standalone-init" strategy="afterInteractive">
+            {`window.ezstandalone = window.ezstandalone || {}; ezstandalone.cmd = ezstandalone.cmd || [];`}
+          </Script>
+        </>
       )}
     </>
   );
